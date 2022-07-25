ef9aa62e51f4f15ab31ee94544cf8f7fc4094665
Grace Chen <103862883+gracchen@users.noreply.github.com>
2022-07-25 15:24:46 -0700

    Records time death + time since starting in csv

--- counter.cpp
+++ counter.cpp
@@ -7,18 +7,19 @@
 #include <ctime> 
 using namespace std;
 
+string diffTime(time_t start, time_t end) {
+  int sec = (int)difftime(end, start);
+  int min = sec / 60;
+  int hours = min / 60;
+  char buffer [10];
+  sprintf(buffer, "%02d:%02d:%02d",int(hours),int(min%60),int(sec%60));
+  return buffer;
+}
+
 int main() 
 {
-    time_t tt;
-    struct tm * ti;
-    time (&tt);
-    ti = localtime(&tt);
-    cout << asctime(ti);
-
-    char output[30];
-    strftime(output, 30, "%m %d %Y, %H:%M:%S\n", ti);
-    cout << output;
-
+    time_t t2;
+    time (&t2);
     ifstream myFile ("count.csv");
     string line;
     size_t total = 0;
@@ -34,11 +35,14 @@ int main()
     int i = 0;
     fstream file;
     file.open("count.csv", std::ios_base::app);
+    time_t tt;struct tm * ti; //initialize
     if (file.is_open()){
       while(line != "q")
       {
         i++;
-        file << endl << line;
+        time (&tt); ti = localtime(&tt);
+        char output[30]; strftime(output, 30, "%m/%d/%Y,%H:%M:%S", ti);
+        file << endl << line << "," << output << "," << diffTime(t2,tt);
         cout << "Death this session: " << i << endl;
         cout << "Death total: " << total + i<< endl;
         getline (cin, line);
