496ce7b02f079c0c45d123c1b04c60eee8a32064
Grace Chen <103862883+gracchen@users.noreply.github.com>
2022-07-25 14:26:33 -0700

    writes input to file and counts rows

--- counter.cpp
+++ counter.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <fstream>
+#include <stdexcept>
 #include <chrono>
 #include <ctime> 
 using namespace std;
@@ -16,13 +19,30 @@ int main()
     strftime(output, 30, "%m %d %Y, %H:%M:%S\n", ti);
     cout << output;
 
-    char input = getchar();
-    int i = 0;
-    while(input != 'q')
-    {
-      i++;
-      cout << "Death this session: " << i << endl;
-      cout << "Death total: " << endl;
-      input = getchar();
+    ifstream myFile ("count.csv");
+    string line;
+    size_t total = 0;
+    if (myFile.is_open()){
+      while (myFile.good()){
+        myFile >> line;
+        cout << line << endl;
+        total++;
+      }
     }
+    total--;
+    getline (cin, line);
+    int i = 0;
+    fstream file;
+    file.open("count.csv", std::ios_base::app);
+    if (file.is_open()){
+      while(line != "q")
+      {
+        i++;
+        file << endl << line;
+        cout << "Death this session: " << i << endl;
+        cout << "Death total: " << total + i<< endl;
+        getline (cin, line);
+      }
+    }
+    file.close();
 }
