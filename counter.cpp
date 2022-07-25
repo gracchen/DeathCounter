60798aa71a67c794ce9f9094c059f191bed5d6fe
Grace Chen <103862883+gracchen@users.noreply.github.com>
2022-07-25 15:52:14 -0700

    Counts deaths per enemy
    ////Console:
    Joe     1
    Malena  3
    Margit  3
    
    Joe
    Death this session: 1   Death total: 8
    Joe     2
    Malena  3
    Margit  3
    
    Margit
    Death this session: 2   Death total: 9
    Joe     2
    Malena  3
    Margit  4
    
    
    ////CSV:
    name,date,time,time-start
    Margit,07/25/2022,15:21:40,00:00:04
    Margit,07/25/2022,15:23:03,00:01:27
    Malena,07/25/2022,15:23:11,00:01:35
    Malena,07/25/2022,15:43:55,00:00:06
    Malena,07/25/2022,15:46:00,00:00:05
    Margit,07/25/2022,15:47:15,00:00:05
    Joe,07/25/2022,15:47:45,00:00:07
    Joe,07/25/2022,15:48:57,00:00:05
    Margit,07/25/2022,15:49:30,00:00:38

--- counter.cpp
+++ counter.cpp
@@ -5,6 +5,8 @@
 #include <stdexcept>
 #include <chrono>
 #include <ctime> 
+#include <stack>
+#include <map>
 using namespace std;
 
 string diffTime(time_t start, time_t end) {
@@ -13,23 +15,51 @@ string diffTime(time_t start, time_t end) {
   int hours = min / 60;
   char buffer [10];
   sprintf(buffer, "%02d:%02d:%02d",int(hours),int(min%60),int(sec%60));
+  //printf("%02d:%02d:%02d\n",int(hours),int(min%60),int(sec%60));
   return buffer;
+}
+
+void printDict(map<string,size_t>& Dict){
+  for(auto it = Dict.cbegin(); it != Dict.cend(); ++it)
+  {
+      cout << it->first << "\t" << it->second << "\n";
+  }
+}
+
+void countNames (stack<string>& nameList, map<string,size_t>& Dict)
+{
+  while (nameList.size() > 1)
+  {
+    string key_to_find = nameList.top();
+    auto it = Dict.find(key_to_find);
+    if (it != Dict.end()) {
+            it->second++;    // increment map's value for key `c`
+        }
+    else {
+      Dict.insert(make_pair(key_to_find, 1));
+    }
+    nameList.pop();
+  }
+  printDict(Dict); cout<< endl;
 }
 
 int main() 
 {
     time_t t2;
-    time (&t2);
+    time (&t2); stack<string> nameList; map<string, size_t> Dict;
     ifstream myFile ("count.csv");
     string line;
     size_t total = 0;
     if (myFile.is_open()){
       while (myFile.good()){
         myFile >> line;
-        cout << line << endl;
+        auto name = line.substr(0, line.find(','));
+        nameList.push(name);
         total++;
       }
     }
+
+    countNames(nameList, Dict);
     total--;
     getline (cin, line);
     int i = 0;
@@ -43,8 +73,9 @@ int main()
         time (&tt); ti = localtime(&tt);
         char output[30]; strftime(output, 30, "%m/%d/%Y,%H:%M:%S", ti);
         file << endl << line << "," << output << "," << diffTime(t2,tt);
-        cout << "Death this session: " << i << endl;
-        cout << "Death total: " << total + i<< endl;
+        cout << "Death this session: " << i << "\t" << "Death total: " << total + i<< endl;
+        nameList.push(line);
+        countNames(nameList, Dict);
         getline (cin, line);
       }
     }
