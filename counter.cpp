14aebbf63fdedcfafd3814eac68232cb6cf18c34
Grace Chen <103862883+gracchen@users.noreply.github.com>
2022-07-25 19:47:48 -0700

    Welcome Msg + fix space in name bug

--- counter.cpp
+++ counter.cpp
@@ -22,7 +22,7 @@ string diffTime(time_t start, time_t end) {
 void printDict(map<string,size_t>& Dict){
   for(auto it = Dict.cbegin(); it != Dict.cend(); ++it)
   {
-      cout << it->first << "\t" << it->second << "\n";
+      cout << it->first << ": " << it->second << "\n";
   }
 }
 
@@ -37,7 +37,7 @@ void writeStats(map<string,size_t>& Dict){
   if (file2.is_open()){
     for(auto it = Dict.cbegin(); it != Dict.cend(); ++it)
     {
-      string line = it->first + "\t" + to_string(it->second) + "\n";
+      string line = it->first + "," + to_string(it->second) + "\n";
       file2 << line;
     }
   }
@@ -51,32 +51,40 @@ void countNames (stack<string>& nameList, map<string,size_t>& Dict)
     string key_to_find = nameList.top();
     auto it = Dict.find(key_to_find);
     if (it != Dict.end()) {
-            it->second++;    // increment map's value for key `c`
+            it->second++;
         }
     else {
       Dict.insert(make_pair(key_to_find, 1));
     }
     nameList.pop();
   }
-  printDict(Dict); cout<< endl;
+  //printDict(Dict); cout<< endl;
 }
 
 int main() 
 {
+  cout << "Welcome to Death Counter. \nEnter an enemy to start, q to quit." << endl;
   time_t t2;
   time (&t2); stack<string> nameList; map<string, size_t> Dict;
   ifstream myFile ("log.csv");
-  string line;
+  string line, name;
   size_t total = 0;
   if (myFile.is_open()){
     while (myFile.good()){
-      myFile >> line;
-      auto name = line.substr(0, line.find(','));
+      getline(myFile, line);
+      //myFile >> line;
+      name = "";
+      for (size_t i = 0; i < line.size(); i++)
+      {
+        if (line[i] == ',') break;
+        name += line[i];
+      }
+      //cout << name << "\n";
       nameList.push(name);
       total++;
     }
   }
-
+  
   countNames(nameList, Dict);
   total--;
   getline (cin, line);
@@ -91,12 +99,15 @@ int main()
       time (&tt); ti = localtime(&tt);
       char output[30]; strftime(output, 30, "%m/%d/%Y,%H:%M:%S", ti);
       file << endl << line << "," << output << "," << diffTime(t2,tt);
-      cout << "Death this session: " << i << "\t" << "Death total: " << total + i<< endl;
       nameList.push(line);
       countNames(nameList, Dict);
+      auto it = Dict.find(line);
+      cout << "Total deaths to " << line << ": " << to_string(it->second) << endl;
+      cout << "Deaths this session: " << i << "\t" << "Total: " << total + i<< endl;
       getline (cin, line);
     }
   }
   file.close();
   writeStats(Dict);
+  printDict(Dict);
 }
