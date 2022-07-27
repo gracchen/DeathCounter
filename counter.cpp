241d6225a7ce25f2f73e6f6edbe41c497c3992d1
Grace Chen <103862883+gracchen@users.noreply.github.com>
2022-07-27 00:16:15 -0700

    p to print stats + implemented victory/death count

--- counter.cpp
+++ counter.cpp
@@ -9,6 +9,12 @@
 #include <map>
 using namespace std;
 
+struct Stats
+{
+  size_t deaths;
+  size_t wins;
+} stats;
+
 string diffTime(time_t start, time_t end) {
   int sec = (int)difftime(end, start);
   int min = sec / 60;
@@ -18,51 +24,76 @@ string diffTime(time_t start, time_t end) {
   return buffer;
 }
 
-void printDict(map<string,size_t>& Dict){
+void printDict(map<string,Stats>& Dict){
   for(auto it = Dict.cbegin(); it != Dict.cend(); ++it)
-      cout << it->first << ": " << it->second << "\n";
+      cout << it->first << ": " << it->second.deaths << ", " << it->second.wins << "\n";
 }
 
-void writeStats(map<string,size_t>& Dict){
+void writeStats(map<string,Stats>& Dict){
   fstream file;
   file.open("count.csv", ios_base::out | std::ios_base::trunc);
   file.close();
 
   fstream file2;
   file2.open("count.csv", std::ios_base::app);
-
+  file2 << "Name,Deaths,Victories\n";
   if (file2.is_open()){
     for(auto it = Dict.cbegin(); it != Dict.cend(); ++it)
     {
-      string line = it->first + "," + to_string(it->second) + "\n";
+      string line = it->first + "," + to_string(it->second.deaths) + "," + to_string(it->second.wins) + "\n";
       file2 << line;
     }
   }
   file2.close();
 }
 
-void countNames (stack<string>& nameList, map<string,size_t>& Dict)
+size_t countNames (stack<string>& nameList, map<string,Stats>& Dict)
 {
+  int deaths = 0;
   while (nameList.size() > 1)
   {
     string key_to_find = nameList.top();
+    bool isWin = false;
+    if (key_to_find[0] == '*')
+    {
+      key_to_find.erase(0,1); 
+      isWin = true;
+    }
+
     auto it = Dict.find(key_to_find);
-    if (it != Dict.end())
-            it->second++;
-    else
-      Dict.insert(make_pair(key_to_find, 1));
+    if (it != Dict.end()){
+      if (isWin)
+        it->second.wins++;
+      else {
+        it->second.deaths++;
+        deaths++;
+      }
+    }
+    else {
+      struct Stats temp;
+      if (isWin)
+        temp = {0,1};
+      else {
+        temp = {1,0};
+        deaths++;
+      }
+      Dict.insert(make_pair(key_to_find, temp));
+    }
     nameList.pop();
   }
+  return deaths;
 }
+
+
 
 int main() 
 {
-  cout << "Welcome to Death Counter. \nEnter enemy upon death, *enemy upon victory, q to quit." << endl;
+  cout << "Welcome to Death Counter. \n[enemy] = death, *enemy = victory, p for stats, q to quit." << endl;
   time_t t2;
-  time (&t2); stack<string> nameList; map<string, size_t> Dict;
+  time (&t2); stack<string> nameList; map<string, Stats> Dict;
   ifstream myFile ("log.csv");
   string line, name;
-  size_t total = 0;
+
   if (myFile.is_open()){
     while (myFile.good()){
       getline(myFile, line);
@@ -75,36 +106,43 @@ int main()
       }
       if (name.size() != 0)
         nameList.push(name);
-      total++;
     }
   }
   
-  countNames(nameList, Dict);
-  total--;
+  size_t total = countNames(nameList, Dict);
   getline (cin, line);
-  int i = 0;
+  int deathCount = 0; int winCount = 0;
   fstream file;
   file.open("log.csv", std::ios_base::app);
   time_t tt;struct tm * ti; //initialize
   if (file.is_open()){
-    file << "\n";
     while(line != "q")
     {
-      i++;
+      if (line == "p") {
+        printDict(Dict); cout << endl;
+      }
+      else if (line.size() != 0) {
       time (&tt); ti = localtime(&tt);
       char output[30]; strftime(output, 30, "%m/%d/%Y,%H:%M:%S", ti);
       file << endl << line << "," << output << "," << diffTime(t2,tt);
       nameList.push(line);
       countNames(nameList, Dict);
+      if (line[0] == '*')
+        line.erase(0,1);
+      else
+        deathCount++;
       auto it = Dict.find(line);
-      cout << "Total deaths to " << line << ": " << to_string(it->second) << endl;
-      cout << "Deaths this session: " << i << "\t" << "Total: " << total + i<< endl;
+      cout << "Deaths to " << line << ": " << to_string(it->second.deaths) << endl;
+      cout << "Victories to " << line << ": " << to_string(it->second.wins) << endl;
+      cout << "Deaths this session: " << deathCount << "\t" << "Total: " << total + deathCount << endl << endl;
+      }
       getline (cin, line);
     }
   }
+  file << "\n";
   file.close();
   writeStats(Dict);
-  printDict(Dict);
-  cout << "Press ENTER to close...";
-  std::cin.get(); 
+  //printDict(Dict);
+  //cout << "Press ENTER to close...";
+  //std::cin.get(); 
 }
